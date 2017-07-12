cf2f505f3bcd41413730062bc93830c65fbc3411
Valtteri Nurminen <valtteri.e.nurminen@aalto.fi>
2017-07-12 23:52:46 +0300

    Fetched restaurant name and courses.

--- src/backend/SodexoDecoder.java
+++ src/backend/SodexoDecoder.java
@@ -1,5 +1,8 @@
 package backend;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,11 +15,26 @@ public class SodexoDecoder implements JSONDecoder {
     private String fetchData(String lang, String restaurantCode) throws Exception{
 
         String date = new SimpleDateFormat("YYYY/MM/dd").format(new Date());
-        return DataFormatter.getJSONData("http://www.sodexo.fi/ruokalistat/output/daily_json/"+restaurantCode+"/"+ date +"/"+lang);
+        return DataFormatter.getJSONData("http://www.sodexo.fi/ruokalistat/" +
+                "output/daily_json/"+restaurantCode+"/"+ date +"/"+lang);
     }
 
     /** Parses the JSON from the given source and returns a corresponding restaurant object.*/
-    public Restaurant parseJSON() throws Exception {
-        return new Restaurant("");
+    public Restaurant parseJSON(String lang, String restaurantCode) throws Exception {
+        String json = fetchData(lang, restaurantCode);
+        JSONObject fullJSON = new JSONObject(json);
+
+        JSONObject meta = fullJSON.getJSONObject("meta");
+        String name = (String) meta.get("ref_title");
+
+        Restaurant restaurant = new Restaurant(name);
+
+        JSONArray courses = fullJSON.getJSONArray("courses");
+
+
+
+        System.out.println(fullJSON.toString(2));
+
+        return restaurant;
     }
 }
