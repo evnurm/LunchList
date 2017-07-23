9c1a7f1c66503853cdad81c19405cecee9e7dcbf
Valtteri Nurminen <valtteri.e.nurminen@aalto.fi>
2017-07-23 16:53:14 +0300

    Clicking on DayButton now loads the corresponding day's menus.

--- src/view/DayButton.java
+++ src/view/DayButton.java
@@ -34,7 +34,9 @@ public class DayButton extends VBox {
     }
     /** onClick handler for the DayButton. */
     public void click(){
-
+       VBox mc = (VBox) getParent().getParent().lookup("#mainContainer");
+        mc.getChildren().clear();
+        mc.getChildren().add(new MenusContainer(idx));
     }
 
 
