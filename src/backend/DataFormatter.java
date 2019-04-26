61c4a682efd631f2609f630011fb5b5afb498e91
Valtteri Nurminen <valtteri.e.nurminen@aalto.fi>
2019-04-26 16:27:07 +0300

    Open and close InputStream from URL.

--- src/backend/DataFormatter.java
+++ src/backend/DataFormatter.java
@@ -1,6 +1,7 @@
 package backend;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
@@ -11,7 +12,8 @@ public class DataFormatter {
     public static String getJSONData(String addr) throws Exception {
 
         URL url = new URL(addr);
-        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        InputStream stream = url.openStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(stream));
 
         String inputLine = in.readLine();
 
@@ -21,6 +23,8 @@ public class DataFormatter {
             json += inputLine + "\n";
             inputLine = in.readLine();
         }
+
+        stream.close();
 
         return json;
 
